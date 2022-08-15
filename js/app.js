4abc9fb27b63fdd6bdc9152c59373293d8922468
Jeffrey Cruz <jeff.jc.cruz@gmail.com>
2022-08-15 23:24:08 +0000

    Fixed bug where active highlight wasn't appearing on active buttons

--- js/app.js
+++ js/app.js
@@ -9,9 +9,29 @@ function ActiveBtn() {
     sectBtn[i].addEventListener('click', function () {
       const currentBtn = document.querySelectorAll('.active-btn');
       currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
-      this.className += 'active-btn';
+      this.className += ' active-btn';
     });
   }
+
+  // Sections active
+  allSections.addEventListener('click', event => {
+    const id = event.target.dataset.id;
+    if (id) {
+      // Remove active from the other buttons
+      sectBtns.forEach(btn => {
+        btn.classList.remove('active');
+      });
+      event.target.classList.add('active');
+
+      // Hide other sections
+      sections.forEach(section => {
+        section.classList.remove('active');
+      });
+
+      const element = document.getElementById(id);
+      element.classList.add('active');
+    }
+  });
 }
 
 ActiveBtn();
